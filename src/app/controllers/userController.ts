d1b4f2ad8cf1b59d64fabbad76d8206a6dbddfa1
firoz.nur <firoznur5@gmail.com>
2025-06-21 21:54:23 +0600

    Add : Zod

--- src/app/controllers/userController.ts
+++ src/app/controllers/userController.ts
@@ -1,9 +1,42 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/userModel";
+import { z } from "zod";
 
 export const userRoutes = express.Router();
 
+
+const CreateUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
+
 userRoutes.post("/create", async (req: Request, res: Response) => {
+
+  try {
+    const body = await CreateUserZodSchema.parseAsync(req.body)
+
+    console.log(body, "zod body");
+
+    const user = await User.create(body);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: user,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
+
   const body = req.body;
   const user = await User.create(body);
   res.status(201).json({
