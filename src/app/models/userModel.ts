5808abd206581aaf28fe22f364c52034f1bb2e5c
firoz.nur <firoznur5@gmail.com>
2025-06-21 17:27:46 +0600

    Add : Custom Validator

--- src/app/models/userModel.ts
+++ src/app/models/userModel.ts
@@ -1,8 +1,13 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/userInterface";
+import validator from "validator";
 
 const userSchema = new Schema<IUser>({
-  firstName: { type: String, required: true, trim: true },
+  firstName: {
+    type: String,
+    required: [true, "first name is required"],
+    trim: true,
+  },
   lastName: { type: String, required: true, trim: true },
   age: {
     type: Number,
@@ -10,9 +15,25 @@ const userSchema = new Schema<IUser>({
     min: [18, "age must be at least 18, got {VALUE}"],
     max: 60,
   },
-  email: { type: String, required: true, lowercase: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: [true, "email must be unique"],
+    // validate: {
+    //   validator: function (value) {
+    //     return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+    //   },
+    //   message: "Invalid email format",
+    // },
+    validate: [validator.isEmail, "Validator email error"],
+  },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
+  role: {
+    type: String,
+    enum: { values: ["user", "admin"], message: "Role is not valid" },
+    default: "user",
+  },
 });
 
 export const User = model("User", userSchema);
